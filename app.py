502f5a33428096e2af83db16417acc067abf62ff
saidino83 <tylorguy2018@gmail.com>
2021-04-24 14:03:13 -0400

    release

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ app =Flask(__name__)
 def index():
     return 'Wellcome to saidino app'
 
-@app.route('/bot', methods=['POST'])
+@app.route('/bot', methods=['POST','GET'])
 def response():
     # recebo o posto mandado por cliente
     # e devolvo para ele o result ai
     query =dict(request.form)['query']
     result = query+" "+time.ctime()
+    if request.method=='GET':
+        return "SO FAZ POST PARA TER A RESPOSTA"
 
     return jsonify(result);
 
