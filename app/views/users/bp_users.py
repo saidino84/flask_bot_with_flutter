ca733807388ba8d1c53ed30362159d4a6f25bed3
saidino83 <tylorguy2018@gmail.com>
2021-06-20 21:24:27 +0200

    reeding db 3

--- app/views/users/bp_users.py
+++ app/views/users/bp_users.py
@@ -7,6 +7,6 @@ users_bp=Blueprint('users_bp',__name__, url_prefix='/users_page')
 
 @users_bp.route("/")
 def index():
-    # adm=User.query.all()[0]
-    adm="sadino PyProgrammer"
-    return f'WELL COME TO USERS BLUEPRINT PAGE <b/>{adm}'
+    adm=User.query.all()[0]
+    # adm="sadino PyProgrammer"
+    return f'WELL COME TO USERS BLUEPRINT PAGE <b/>{adm.username}'
